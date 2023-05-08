547a9e796163d52b1bbea6ed28d4db02b124c913
Hamza Haque <hamzahaque30@gmail.com>
2023-05-08 13:25:07 -0500

    Add comments of an overall plan

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hi");
+// Get User input for street
+// Get all matching results for street name
+// Render streets in the aside menu
+
+// When user clicks on a street
+// Get the info about upcoming bus
+// Render
