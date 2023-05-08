f0826d75bb1ccdda43f27f5d44b10413b5370772
Hamza Haque <hamzahaque30@gmail.com>
2023-05-08 14:41:40 -0500

    Get street names

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { config } from "./config.js";
+const baseEndPoint = "https://api.winnipegtransit.com/v3/";
+const api_endpoint = `?api-key=${config.apiKey}`;
 
 // Get User input for street
 // Get all matching results for street name
@@ -8,7 +10,18 @@ import { config } from "./config.js";
 // Get the info about upcoming bus
 // Render
 
-function getStreets(streetName) {}
+async function fetchStreets(streetName) {
+  const streetEndPoint = `${baseEndPoint}streets.json${api_endpoint}&name=${streetName}`;
+  const response = await fetch(streetEndPoint);
+  const data = await response.json();
+  return data;
+}
+
+function getStreets(streetName) {
+  fetchStreets(streetName).then((data) => {
+    renderHTML(data.streets);
+  });
+}
 
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
