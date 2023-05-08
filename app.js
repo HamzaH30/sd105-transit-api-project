f4c5fe9b7313557e2219929808411dd196802eb3
Hamza Haque <hamzahaque30@gmail.com>
2023-05-08 13:48:20 -0500

    Add API Key

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { config } from "./config.js";
+
 // Get User input for street
 // Get all matching results for street name
 // Render streets in the aside menu
@@ -5,3 +7,10 @@
 // When user clicks on a street
 // Get the info about upcoming bus
 // Render
+
+function getStreets(streetName) {}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  getStreets(event.target.children[0].value);
+});
